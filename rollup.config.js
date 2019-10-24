642ef6ee86ac9000c96fde2e0d366049cc00e29e
BSKY <git@bsky.moe>
2019-10-24 21:10:46 +0900

    Add lebab plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,24 @@
 import compiler from '@ampproject/rollup-plugin-closure-compiler'
+import lebab from '@imbsky/rollup-plugin-lebab'
 import resolve from 'rollup-plugin-node-resolve'
 import { terser } from 'rollup-plugin-terser'
+
+const lebabOption = [
+  'arrow',
+  'arrow-return',
+  'for-of',
+  'for-each',
+  'arg-rest',
+  'arg-spread',
+  'obj-method',
+  'obj-shorthand',
+  'no-strict',
+  'exponent',
+  'multi-var',
+]
 
 export default {
   input: 'lib/es6/ocaml/sigma.js',
   output: { file: 'javascript/sigma.js', format: 'es' },
-  plugins: [resolve(), compiler(), terser()],
+  plugins: [compiler(), lebab(lebabOption), resolve(), terser()],
 }
