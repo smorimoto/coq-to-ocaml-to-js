87d9616488075fc34a19981a4a16895a61991b97
BSKY <git@bsky.moe>
2019-10-24 14:09:55 +0900

    Tweaks

--- coq/sigma.v
+++ coq/sigma.v
@@ -17,20 +17,20 @@ limitations under the License.
 Require Extraction.
 
 Fixpoint f n :=
- match n with
- | O => nat
- | S n => nat -> (f n)
- end.
+  match n with
+  | O => nat
+  | S n => nat -> (f n)
+  end.
 
 Definition sigma: forall n, f n.
- intro n; induction n; simpl.
-  exact O.
- intro m.
- destruct n; simpl in *.
-  exact m.
- intro o.
- apply IHn.
- exact (m+o).
+  intro n; induction n; simpl.
+    exact O.
+  intro m.
+  destruct n; simpl in *.
+    exact m.
+  intro o.
+  apply IHn.
+  exact (m+o).
 Defined.
 
 Extraction "sigma.ml" sigma.
