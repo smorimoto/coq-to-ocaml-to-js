6a878841887c5af2c343be377f6b604e63e58e85
BSKY <git@bsky.moe>
2019-11-01 02:05:02 +0900

    Remove lebab

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,9 @@
 import compiler from '@ampproject/rollup-plugin-closure-compiler'
-import lebab from '@imbsky/rollup-plugin-lebab'
 import resolve from 'rollup-plugin-node-resolve'
 import { terser } from 'rollup-plugin-terser'
-
-const lebabOption = [
-  'arrow',
-  'arrow-return',
-  'for-of',
-  'for-each',
-  'arg-rest',
-  'arg-spread',
-  'obj-method',
-  'obj-shorthand',
-  'no-strict',
-  'exponent',
-  'multi-var',
-]
 
 export default {
   input: 'lib/es6/ocaml/sigma.js',
   output: { file: 'javascript/sigma.js', format: 'es' },
-  plugins: [compiler(), lebab(lebabOption), resolve(), terser()],
+  plugins: [compiler(), resolve(), terser()],
 }
