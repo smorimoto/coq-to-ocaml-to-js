7403826fe36028a8c8ba6cfcd9b03887ac2b494a
BSKY <git@bsky.moe>
2019-11-12 11:31:11 +0900

    Remove rollup-plugin-node-resolve

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,8 @@
 import compiler from '@ampproject/rollup-plugin-closure-compiler';
-import resolve from 'rollup-plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
 
 export default {
   input: 'lib/es6/ocaml/sigma.js',
   output: { file: 'javascript/sigma.js', format: 'es' },
-  plugins: [resolve(), compiler(), terser()],
+  plugins: [compiler(), terser()],
 };
